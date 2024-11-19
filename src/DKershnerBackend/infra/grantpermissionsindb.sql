6da49e0a104269ad31aea8337bb04ee1c83d2781
Derek Kershner <dkershner@dkershner.com>
2024-11-19 15:26:25 -0800

    fix: corrected sql script

--- src/DKershnerBackend/infra/grantpermissionsindb.sql
+++ src/DKershnerBackend/infra/grantpermissionsindb.sql
@@ -1,5 +1,5 @@
-CREATE USER [dk-webapp] FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER [dk-webapp];
-ALTER ROLE db_datawriter ADD MEMBER [dk-webapp];
-ALTER ROLE db_ddladmin ADD MEMBER [dk-webapp];
+CREATE USER [dk-db-access] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [dk-db-access];
+ALTER ROLE db_datawriter ADD MEMBER [dk-db-access];
+ALTER ROLE db_ddladmin ADD MEMBER [dk-db-access];
 GO
