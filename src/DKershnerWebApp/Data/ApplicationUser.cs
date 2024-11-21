b5a5f1f8f844224adfa7efb1d8990912334d591b
Derek Kershner <dkershner@dkershner.com>
2024-11-21 09:15:40 -0800

    feat: Interactive project

--- src/DKershnerWebApp/Data/ApplicationUser.cs
+++ src/DKershnerWebApp/Data/ApplicationUser.cs
@@ -1,10 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 
-namespace DKershnerWebApp.Data
-{
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-    }
+namespace DKershnerWebApp.Data;
 
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
 }
+
